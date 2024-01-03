3eec5cf77755f6e4c473521c0fb03c87e1c77e57
Bijoy Kar <bijoypoco12345@gmail.com>
2024-01-03 16:54:45 +0530

    [feat]: Sm NavBar added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
-import NavBar from "./NabBar";
+import NavBar from "./NavBar";
+import SmNavBar from "./SmNavBar";
 import { BsGithub } from "react-icons/bs";
 import BrandLogo from "../Brand/BrandLogo";
 
 export default function Header() {
   return (
     <header className="px-6 py-2 flex justify-between items-center h-16 ">
-      <Link href="/">
-        <BrandLogo />
-      </Link>
-      <NavBar />
-      <div className="">
+      <div className="flex items-center gap-3">
+        <SmNavBar />
+        <Link href="/">
+          <BrandLogo />
+        </Link>
+      </div>
+      <div className="flex items-center gap-4">
+        <NavBar />
+        <span className="w-[2px] h-8 border hidden sm:block"></span>
         <Link href="https://github.com/TechStarHub/bio-drop" className="">
           <BsGithub className="text-2xl" />
         </Link>
--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="flex gap-3">
+    <nav className="hidden sm:flex gap-3 ">
       <Link href="/">
-        <span className="text-sm font-medium">Home</span>
+        <span className="text-md font-medium">Home</span>
       </Link>
       <Link href="/people">
-        <span className="text-sm font-medium">People</span>
+        <span className="text-md font-medium">People</span>
       </Link>
       <Link href="/docs/quick-start">
-        <span className="text-sm font-medium">Docs</span>
+        <span className="text-md font-medium">Docs</span>
       </Link>
     </nav>
   );
