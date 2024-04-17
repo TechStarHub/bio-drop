86f632c9898c8b2f389fed9b18f627df33e582f2
Bijoy Kar <bijoykar54321@gmail.com>
2024-04-17 11:43:17 +0530

    [fix]: Profile link icon case sensitive removed

--- src/components/People/PeopleProfile/Links/PeopleLink.tsx
+++ src/components/People/PeopleProfile/Links/PeopleLink.tsx
@@ -33,7 +33,7 @@ export default async function PeopleLink({
   type,
   key,
 }: PeopleLinkProps) {
-  const Icon = await IconImporter(type);
+  const Icon = await IconImporter(type.toLowerCase());
   return (
     <div className="w-full flex justify-between items-center gap-4 px-4 py-2 rounded-full bg-slate-200 border-2 hover:border-sky-600 transition-all duration-400 sm:w-[500px] md:w-[600px] lg:w-[700px] xl:w-[800px] 2xl:w-[1100px] max-w-[1200px]">
       <span className="flex items-center gap-4 w-full">
