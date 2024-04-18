15390ef95d9a21cd9f8a4816e17393df8c279af2
Bijoy Kar <bijoykar54321@gmail.com>
2024-04-18 20:07:52 +0530

    [refactor]: IconImporter func moved to utils

--- src/components/People/PeopleProfile/Links/PeopleLink.tsx
+++ src/components/People/PeopleProfile/Links/PeopleLink.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import CopyBtn from "../CopyBtn/CopyBtn";
+import { IconImporter } from "@/utils";
 
 type PeopleLinkProps = {
   label: string;
@@ -7,33 +8,6 @@ type PeopleLinkProps = {
   type: string;
   key: number;
 };
-
-async function IconImporter(type: string) {
-  switch (type) {
-    case "twitter":
-      return import("react-icons/fa6").then((module) => module.FaTwitter);
-    case "github":
-      return import("react-icons/fa6").then((module) => module.FaGithub);
-    case "linkedin":
-      return import("react-icons/fa6").then((module) => module.FaLinkedin);
-    case "instagram":
-      return import("react-icons/fa6").then((module) => module.FaInstagram);
-    case "facebook":
-      return import("react-icons/fa6").then((module) => module.FaFacebook);
-    case "youtube":
-      return import("react-icons/fa6").then((module) => module.FaYoutube);
-    case "reddit":
-      return import("react-icons/fa").then((module) => module.FaRedditAlien);
-    case "tiktok":
-      return import("react-icons/fa6").then((module) => module.FaTiktok);
-    case "snapchat":
-      return import("react-icons/fa").then((module) => module.FaSnapchatGhost);
-    case "discord":
-      return import("react-icons/fa6").then((module) => module.FaDiscord);
-    default:
-      return import("react-icons/fa6").then((module) => module.FaLink);
-  }
-}
 
 export default async function PeopleLink({
   label,
